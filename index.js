3ff706dc9d634b82376efe90a71fa10c65f27f3d
Abraham <abukip344@gmail.com>
2024-03-25 04:31:16 -0400

    superbowl find method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+// superbowlWin function
+function superbowlWin(record){
+    if (record.find(record => record.result === "W")){
+        return record.find(record => record.result === "W").year
+    }
+    else{
+        return undefined
+    }
+}
+
+console.log(superbowlWin(record))
